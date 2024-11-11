5e881594b0a40f4af60425b2699d432d210e7849
julio cesar <jcesartrabajo@gmail.com>
2024-11-10 21:45:15 -0700

    remove clean

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/service', methods=['GET'])
+@app.route('/data', methods=['GET'])
 def service():
     return jsonify({"message": "Hello from Service Number 1!"})
 
